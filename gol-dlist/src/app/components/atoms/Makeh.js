ae520cbeea9b9316dcdb298e88651f7af5360ada
William Burroughs <williamburroughs@Williams-MBP.localdomain>
2023-10-19 14:37:50 -0400

    Current Verison

---
+++ gol-dlist/src/app/components/atoms/Makeh.js
@@ -0,0 +1,7 @@
+
+
+export default function Makeh(props){
+    return (
+      <h1 className = {props.passclasses}>{props.textToPass}</h1>
+    )
+  }
--- gol-dlist/src/app/page.js
+++ gol-dlist/src/app/page.js
@@ -1,19 +1,17 @@
+"use client"
 import Image from 'next/image'
 import styles from './page.module.css'
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Makep from './components/atoms/Makep'
+import Makeh from './components/atoms/Makeh'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-        <body className={styles.myBody}>
-
-          <Makep
-            className = ""
-            textToPass = "Hello there"
-          />
-
-        </body>
+      <body className={styles.myBody}>
+        <Makep className="" textToPass="Hello there" />
+        <Makeh className="" textToPass="Hello there" />
+      </body>
     </main>
   )
 }
